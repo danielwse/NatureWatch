0b3e6fccdfcc77ac0be4bc2f313f6169933f8bc6
Xufangzhou <e0376982@u.nus.edu>
2021-03-21 23:25:18 +0800

    Added quiz cover

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuizCover></QuizCover>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuizCover from './components/QuizCover.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuizCover
   }
 }
 </script>
---
+++ src/components/QuizCover.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <span class="bigWhite">Welcome to </span>
+        <span class="lightGreen">Play to Plant!</span><br>
+        <span class="green">for every 5 questions that you answer correctly NatureWatch donates the costs<br>
+            required to plant a tree in the Amazon Rainforest!</span><br><br>
+        <span class="green">In collaboration with</span> nbsp
+        <img src="../assets/oneTreePlanted_logo.png" alt="Not found" style="width:168px"><br>
+        <span class="smallWhite">Click me to start planting trees!</span> nbsp
+        <img src="../assets/plant.svg" style="width:400px">
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuizCover',
+    data() {
+        return {
+
+        }
+    },
+    created:function() {
+        document.body.style.backgroundColor = "#343434"
+    }
+}
+</script>
+
+<style scoped>
+    * {
+        font-family: "Mohave"
+    }
+    .bigWhite {
+        color:#FFFFFF;
+        font-size: 64px;
+    }
+    .smallWhite {
+        color:#FFFFFF;
+        font-size:24px;
+        vertical-align: bottom;
+        line-height:400px;
+    }
+    .lightGreen{
+        color: #4FE11C;
+        font-size: 64px;
+    }
+    .green{
+        color: #A2EA89;
+        font-size:24px;
+    }
+</style>
