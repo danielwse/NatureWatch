2bfa4a048ae5c9b54e5aafaab704c3abc238f0b2
wzker11 <wzker11@gmail.com>
2021-03-28 00:55:53 +0800

    new nav with search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <ul>
             <li><router-link to="/" exact>Home</router-link></li>
             <li><router-link to="/play" exact>Play to Plant!</router-link></li>
@@ -19,30 +20,34 @@
                     <router-link to="/animal_v" exact>Vulnerable</router-link>
                 </div>
             </li>
+            
+            <div class="search-container">
+                <input type="text" placeholder="Search.." name="search">
+            </div>
         </ul>
     </div>
 </template>
 
 <script>
 
-export default {
-   
-}
+  export default {
+    
+  }
 </script>
 
 <style scoped>
 ul{
     list-style-type: none;
-    margin: 0;
+    margin:0;
     padding: 0;
     overflow: hidden;
-    margin-top:0px ;
+    margin-top:0px;
     margin-left:0px;
 }
 .dropbtn{
   display: inline-block;
   color: black;
-  font-size:2em;
+  font-size:1.3em;
   text-align: left;
   margin-top: 50px;
   padding: 0px 16px;
@@ -50,13 +55,15 @@ ul{
 }
 li{
   float:left;
+  margin-left:55px;
 }
 li a{
   display: inline-block;
   color: black;
   text-align: left;
   margin-top: 50px;
-  font-size:2em;
+  font-size:1.3em;
+  font-weight: 500;
   padding: 0px 16px;
   text-decoration: none;
 }
@@ -71,11 +78,11 @@ li.dropdown {
   position: absolute;
   background-color: white;
   font-size: 0.7em;
+  font-weight: 500;
   min-width: 150px;
   min-height:10px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
   z-index: 1;
-  
 }
 .dropdown-content a{
   color: black;
@@ -93,4 +100,16 @@ li.dropdown {
 li.router-linkactive{
     color: white;
 }
+.search-container {
+  float: right;
+}
+input[type=text] {
+  padding: 6px;
+  margin-top: 30px;
+  margin-right:40px;
+  font-size: 17px;
+  border: none;
+}
+
+
 </style>>
