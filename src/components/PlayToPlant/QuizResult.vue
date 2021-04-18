af56eeb27b4918b0497f3709c977605d412cbf5e
Fangzhou26 <xufangzhou07@gmail.com>
2021-04-18 16:31:53 +0800

    Fixed bugs on quizResult refresh and redirection to homepage

--- src/components/PlayToPlant/QuizResult.vue
+++ src/components/PlayToPlant/QuizResult.vue
@@ -2,21 +2,28 @@
     <div>
         <PlayHeader></PlayHeader><br>
         <div>
-            <div v-show="counter==5">
-                <div class="bigWhite">Congratulations!</div>
-                <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> Your scored </span>
-                <span v-bind:style="{color:'#9BDF84', fontSize:'28px'}">{{counter}}/5 </span>
-                <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> and planted 1 tree!</span><br>
-                <br><br><br><br>
-            </div>
+            <div v-show="counter||counter==0">
+                <div v-show="counter==5">
+                    <div class="bigWhite">Congratulations!</div>
+                    <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> Your scored </span>
+                    <span v-bind:style="{color:'#9BDF84', fontSize:'28px'}">{{counter}}/5 </span>
+                    <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> and planted 1 tree!</span><br>
+                    <br><br><br><br>
+                </div>
 
-            <div v-show="counter!=5">
-                <div class="bigWhite">Try again!</div><br>
-                <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> Your scored </span>
-                <span v-bind:style="{color:'#9BDF84', fontSize:'28px'}">{{counter}}/5</span>
-                <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}">! {{5-counter}} more point(s) to plant 1 tree!</span><br>
-                <br><br><br>
-            </div>  
+                <div v-show="counter!=5">
+                    <div class="bigWhite">Try again!</div><br>
+                    <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}"> Your scored </span>
+                    <span v-bind:style="{color:'#9BDF84', fontSize:'28px'}">{{counter}}/5</span>
+                    <span v-bind:style="{color:'#FFFFFF', fontSize:'28px'}">! {{5-counter}} more point(s) to plant 1 tree!</span><br>
+                    <br><br><br>
+                </div> 
+            </div> 
+            
+            <div class = "undefined bigWhite" v-show="!counter&&counter!=0">
+                <div v-show="chances<=0">Click the seedling to return to homepage</div>
+                <div v-show="chances>0">Click the seedling to try again</div>
+            </div>
 
             <Floating></Floating>
         </div>
@@ -51,19 +58,29 @@ export default {
     methods:{
         redirect:function() {
             if (this.chances>0) this.$router.push('/Questions');
+            else this.$router.push('/playtoplant');
         },
     },
     created:function() {
         document.body.style.backgroundColor = "#343434";
-        var uid=firebase.auth().currentUser.uid;
-        database.collection('Users').doc(uid).onSnapshot({includeMetadataChanges:true}, (doc)=> {
-            this.chances = doc.data().chanceLeft;
-        });
+        const self = this;
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            var uid=firebase.auth().currentUser.uid;
+            database.collection('Users').doc(uid).onSnapshot({includeMetadataChanges:true}, (doc)=> {
+                self.chances = doc.data().chanceLeft;
+                console.log(self.chances);
+            });
+          }
+        })
     }
 }
 </script>
 
 <style scoped>
+    .undefined {
+        height:220px;
+    }
     .bigWhite {
         color:#FFFFFF;
         font-size: 64px;
